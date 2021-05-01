35bf13121d33522b7594414ceb06eace30dabb05
Lorenzo <templarec@gmail.com>
2021-05-01 23:23:11 +0200

    v 1.0

---
+++ getRepos.php
@@ -0,0 +1,42 @@
+<?php
+
+
+if (isset($_GET['user'])) {
+    $user = $_GET['user'];
+
+}
+    $token = 'afd130c21f2a456536ad1634445f0aab2413477a';
+    $header = array(
+        'User-Agent: request',
+        'Authorization: '.$token.'',
+    );
+    $url = 'https://api.github.com/search/repositories?q=user:'.$user."&per_page=100";
+    $gitPath = $_GET['gitpath'];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+    //$response = json_decode($response);
+
+
+    //$numeroRepo =  $response->total_count;
+    //echo $numeroRepo;
+    echo $response ;
+
+    /*foreach ($response->items as $keys => $values ) {
+        $dir = 'output/'. $user . '/'. $values->name;
+        $command = $gitPath." clone " . "$values->clone_url".' '. $dir;
+        //shell_exec($command);
+
+    }*/
+
+
+
+
+
+?>
---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue ({
+	el: '#root',
+	data: {
+		
+	},
+	computed: {
+
+	},
+	methods: {
+
+	}
+});
