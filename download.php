3868a4ae3c19e8bff056d3dbd59611ab82d10aef
Lorenzo <templarec@gmail.com>
2021-05-02 00:46:39 +0200

    v 1.104 - fix output curl

--- download.php
+++ download.php
@@ -4,6 +4,6 @@ $repo = $_GET['repo'];
 $repoUrl = $_GET['repourl'];
 $gitPath = $_GET['gitpath'];
 $dir = 'output/'. $user . '/'. $repo;
-$command = $gitPath." clone " . "$repoUrl".' '. $dir;
+$command = $gitPath." clone " . "$repoUrl".' '. $dir." 2>&1";
 $risposta = shell_exec($command);
 echo $risposta;
